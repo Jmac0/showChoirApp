02624bf6e3b36add07ff1215e4a5369b25ce667f
JamieMac <Jamiejmackenzie@gmail.com>
2022-09-22 12:37:52 +0100

    Setting up screens authenticated or non-authenticated users

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MemberQrCode } from './MemberQrCode';
 import { Account } from './Account';
 
 export const BottomTabsNavigator: React.FC = () => {
   const BottomTabs = createBottomTabNavigator();
+  const [isAuth, setIsAuth] = useState(true);
   return (
     <BottomTabs.Navigator
       screenOptions={({}) => ({
         headerTitleStyle: { fontSize: 20 },
       })}>
-      <BottomTabs.Screen
-        name="Home"
-        component={MemberQrCode}
-        options={{ title: 'Membership' }}
-      />
-      <BottomTabs.Screen
-        name="Account"
-        component={Account}
-        options={{ title: 'Account' }}
-      />
+      {isAuth ? (
+        <BottomTabs.Screen
+          name="Home"
+          component={MemberQrCode}
+          options={{ title: 'Membership' }}
+        />
+      ) : (
+        /*screens for create account & forgot password ect */
+        <BottomTabs.Screen
+          name="Account"
+          component={Account}
+          options={{ title: 'Account' }}
+        />
+      )}
     </BottomTabs.Navigator>
   );
 };
